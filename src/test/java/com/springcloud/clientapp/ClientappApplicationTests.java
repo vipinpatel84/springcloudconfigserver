f4d16c39270593a73eb6b7c657df04989bb6dc73
vipin <vipin.vipinpatel84@gmail.com>
2021-08-10 22:23:20 +0530

    initial Cloud Config

---
+++ src/test/java/com/springcloud/clientapp/ClientappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.clientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
